13c8c24f2c5efee2868d5c9d36b8bdd137789db9
2191867E <jasonegan59@gmail.com>
2020-05-27 16:58:53 +0100

    first commit of random snake game

---
+++ snake_game.py
@@ -0,0 +1,66 @@
+import turtle
+import time
+
+delay = 0.1
+
+# screen set up
+
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer() # turns of the screen updates
+
+# snake creation
+
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# functions
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_right():
+    head.direction = "right"
+
+def go_left():
+    head.direction = "left"
+
+def move():
+    if head.direction == "up":
+        head.sety(head.ycor() + 20)
+    if head.direction == "down":
+        head.sety(head.ycor() - 20)
+    if head.direction == "right":
+        head.setx(head.xcor() + 20)
+    if head.direction == "left":
+        head.setx(head.xcor() - 20)
+
+# keyboard bindings
+
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_right, "d")
+wn.onkeypress(go_left, "a")
+
+# main game loop
+while True:
+    wn.update()
+    move()
+
+    time.sleep(delay)
+
+
+
+
+
+wn.mainloop()
